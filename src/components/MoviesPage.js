4eb8838a0fad9e09a1c6db57fb48c8b2cfd92595
git_bnderitu <bnnderitu01@email.com>
2023-01-17 19:34:55 +0300

    Modified the app component

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -10,7 +10,6 @@ function MoviesPage({ movies }) {
   // useRouteMatch returns a special object with information about
   // the currently matched route
   const match = useRouteMatch();
-  console.log(match);
 
   return (
     <div>
@@ -19,8 +18,14 @@ function MoviesPage({ movies }) {
         we can use the current URL from the `match` object as part of the path;
         this will generate a url like "/movies/:movieId"
       */}
+      {/* Adding code to show a message to the user to select a movie if they haven't yet */}
+      <Route exact path={match.url}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      
       <Route path={`${match.url}/:movieId`}>
-        <MovieShow />
+        {/* adding the movies object as a prop to MovieShow */}
+        <MovieShow movies={movies} />
       </Route>
     </div>
   );
